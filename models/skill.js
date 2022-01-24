b0a46f591c59045159679beda06d3979f3f45e3f
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-24 15:38:13 -0600

    update skill model

--- models/skill.js
+++ models/skill.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const skillSchema = new mongoose.Schema({
-    skill: {type: String},
-    level: {type: String}
+    skill: String,
+    level: String
 },{ timestamps: true });
 
 const Skill = mongoose.model('Skill', skillSchema);
