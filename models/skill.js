cf1961afa590f074e7375f942f6296ea2cb5aa34
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-24 15:16:37 -0600

    skills Schema finalized

--- models/skill.js
+++ models/skill.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const skillSchema = new mongoose.Schema({
     skill: {type: String},
     level: {type: String}
-});
+},{ timestamps: true });
 
 const Skill = mongoose.model('Skill', skillSchema);
 
